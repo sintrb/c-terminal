121a3a919844536a02acaf606cbd97f9dbe994d6
RobinTang <sintrb@gmail.com>
2015-01-08 13:21:21 +0800

    添加示例

--- demo.c
+++ demo.c
@@ -6,9 +6,12 @@ int main()
 {
 	
 	term_clear();	// 清屏
-	
+	term_color(47, 31);	// 设置背景色为白色 字体颜色为深红
 	printf("Hello World\n");
+	term_coloroff();	// 关闭颜色
 	
+	term_goto(5,8);	// 跳转到5行8列位置
+	printf("I'm Row=5 Col=8");
 	
 	
 	
